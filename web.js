d93daa323f838593c637b6280c4e1a845c92a06e
Ubuntu <ubuntu@ip-172-31-42-63.us-west-2.compute.internal>
2013-07-10 08:01:02 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,22 +3,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var content = 'Treta';
-
 var buf = new Buffer(16);
-
-fs.readFile('./index.html', function read(err, data) {
-    if (err) {
-        throw err;
-    }
-    content = data;
-
-});
 
 
 app.get('/', function(request, response) {
   buf.write('AJ', 'utf-8');
-  response.send(buf.toString('utf-8');
+  response.send(buf.toString('utf-8'));
 });
 
 
